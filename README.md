541976aa6b966e2010e4c6e196548f15f9127b6d
Radostin Marinov <86836160+rmarinov045@users.noreply.github.com>
2022-01-29 21:08:23 +0200

    Bug fixes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Portfolio_website
-Personal portfolio website
+# Radostin Marinov's personal portfolio website
+
+Hi,
+
+This is my personal portfolio website, built using VueJS and TailiwindCSS. 
+
+It includes info about me, my skillset and some demos to get to know what I do.
+
+Feel free to leave any feedback!
+
+Thank you!
--- front/src/components/contact_component.vue
+++ front/src/components/contact_component.vue
@@ -11,20 +11,20 @@
         </div>
       </div>
       <div class="">
-        <form class="flex flex-col gap-8" id="contact-form" name="contact-form">
+        <form class="flex flex-col gap-8" id="contact-form-mobile" name="contact-form">
 
           <div class="flex w-full gap-10">
             <div class="flex flex-col w-full">
               <label for="fname" class="font-bold text-left">First name*</label>
               <input type="text" placeholder="John" id="fname"
                      class="tranform transition ease-in-out 150 border-2 border-solid rounded p-2 mt-2 outline-none focus:border-green-500"
-                     required form="contact-form" v-model="firstName">
+                      form="contact-form" v-model="firstName">
             </div>
             <div class="flex flex-col w-full">
               <label for="lname" class="font-bold text-left">Last name*</label>
               <input type="text" placeholder="Wick" id="lname"
                      class="tranform transition ease-in-out 150 border-2 border-solid rounded p-2 mt-2 outline-none focus:border-green-500"
-                     required form="contact-form" v-model="lastName">
+                      form="contact-form" v-model="lastName">
             </div>
           </div>
 
@@ -32,19 +32,19 @@
             <label for="email" class="font-bold text-left">Email*</label>
             <input type="email" placeholder="lorem@ipsum.com" id="email"
                    class="tranform transition ease-in-out 150 border-2 border-solid rounded p-2 mt-2 outline-none focus:border-green-500"
-                   required form="contact-form" v-model="email">
+                    form="contact-form" v-model="email">
           </div>
 
           <div class="flex flex-col">
             <label for="message" class="font-bold text-left">Message*</label>
-            <textarea v-model="message" form="contact-form" placeholder="Please enter your message here..." required
+            <textarea v-model="message" form="contact-form" placeholder="Please enter your message here..."
                       id="message" rows="6" cols="10"
                       class="tranform transition ease-in-out 150 border-2 border-solid rounded p-2 mt-2 outline-none focus:border-green-500"></textarea>
           </div>
 
           <div class="flex w-full justify-center">
-            <button @click="collectFormData" ref="submit" type="submit"
-                    class="hover:opacity-80 transform transition ease-in-out 150 font-bold bg-black text-white p-2 w-1/2 focus:outline-none focus:ring-2 focus:ring-green-500">
+            <button id="submit-button-mobile" @click="collectFormData" ref="submit" type="submit"
+                    class="hover:opacity-80 transform transition ease-in-out 150 font-bold bg-black text-white p-2 w-1/2 focus:outline-none">
               Submit
             </button>
           </div>
@@ -62,7 +62,7 @@
       </div>
       <div class="mt-10 flex justify-center w-full mb-10">
         <a class="hover:opacity-80 transform transition ease-in-out 150 font-bold bg-black text-white p-2 w-1/2 focus:outline-none focus:ring-2 focus:ring-green-500"
-           href="mailto:">Send email via your own client!</a>
+           href="mailto:r.marinov045@gmail.com">Send email via your own client!</a>
       </div>
     </div>
 
@@ -158,12 +158,24 @@ export default {
       // Basic validation
       const entries = [this.firstName, this.lastName, this.message, this.email]
       const button = document.getElementById('submit-button')
+      const mobileButton = document.getElementById('submit-button-mobile')
 
       for (let entry of entries) {
         if (entry.length === 0) {
           button.style.backgroundColor = '#fd0000'
           button.innerText = 'Please fill in all fields!'
           button.style.cursor = 'default'
+          mobileButton.style.backgroundColor = '#fd0000'
+          mobileButton.innerText = 'Please fill in all fields'
+          mobileButton.style.cursor = 'default'
+          setTimeout(() => {
+            button.style.backgroundColor = '#000000'
+            button.innerText = 'Submit'
+            button.style.cursor = 'pointer'
+            mobileButton.style.backgroundColor = '#000000'
+            mobileButton.innerText = 'Submit'
+            mobileButton.style.cursor = 'pointer'
+          }, 4000)
           return
         }
       }
@@ -171,6 +183,14 @@ export default {
       if (!this.email.includes('@') || this.email.indexOf('@') === this.email.length - 1) {
         button.style.backgroundColor = '#fd0000'
         button.innerText = 'Please fill in a correct email address!'
+        mobileButton.style.backgroundColor = '#fd0000'
+        mobileButton.innerText = 'Please fill in a correct email address!'
+        setTimeout(() => {
+          button.style.backgroundColor = '#000000'
+          button.innerText = 'Submit'
+          mobileButton.style.backgroundColor = '#000000'
+          mobileButton.innerText = 'Submit'
+        }, 4000)
         return
       }
 
@@ -182,15 +202,22 @@ export default {
       }
       this.$emit('submitForm', data)
       document.getElementById('contact-form').reset()
+      document.getElementById('contact-form-mobile').reset()
 
       button.style.backgroundColor = '#4caf50'
       button.innerText = 'Success!'
       button.disabled = true
+      mobileButton.style.backgroundColor = '#4caf50'
+      mobileButton.innerText = 'Success!'
+      mobileButton.disabled = true
 
       setTimeout(() => {
         button.style.backgroundColor = '#000000'
         button.innerText = 'Submit'
         button.disabled = false
+        mobileButton.style.backgroundColor = '#000000'
+        mobileButton.innerText = 'Submit'
+        mobileButton.disabled = false
       }, 4000)
     }
   },
